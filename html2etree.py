91eff4936bbb9b7efc6965f183a9bf7e34e55469
Artur de Sousa Rocha <adsr@poczta.onet.pl>
2014-05-30 21:59:15 +0200

    Draft implementation - untested

--- html2etree.py
+++ html2etree.py
@@ -1,5 +1,6 @@
 from html.entities import entitydefs
 from html.parser import HTMLParser
+from io import StringIO
 import xml.etree.ElementTree as ET
 
 
@@ -7,35 +8,51 @@ class HTML2ETree(HTMLParser):
 
     def __init__(self):
         HTMLParser.__init__(self)
-        self._tree = ET.ElementTree()
+        self._tree = None
         self._stack = []
+        self._text = None
 
     def handle_starttag(self, tag, attrs):
-        attrs = dict(attrs)
+        self.handle_startendtag(tag, attrs)
 
     def handle_endtag(self, tag):
-        pass
+        assert self._tree.pop().tag == tag
 
     def handle_startendtag(self, tag, attrs):
+        if self._text:
+            self._stack[-1].text = self._text.getvalue()
+            self._text = None
         attrs = dict(attrs)
+        if self._stack:
+            ET.SubElement(self._stack[-1], tag, attrs)
+        else:
+            self._stack.append(ET.Element(tag, attrs))
+            self._tree = ET.ElementTree(self._stack[-1])
+        self._text = StringIO()
 
     def handle_data(self, data):
-        pass
+        if self._text == None:
+            self._text = StringIO()
+        self._text.write(data)
 
     def handle_entityref(self, name):
-        pass
+        self.handle_data(entitydefs[name])
 
     def handle_charref(self, name):
-        pass
+        if name.startswith('x'):
+            codepoint = int(name[1:], 16)
+        else:
+            codepoint = int(name)
+        self.handle_data(chr(codepoint))
 
     def handle_comment(self, data):
-        pass
+        self._stack[-1].append(ET.Comment(data))
 
     def handle_decl(self, decl):
         pass
 
     def handle_pi(self, data):
-        pass
+        self._stack[-1].append(ET.ProcessingInstruction(data))
 
     def close(self):
         super().close()
