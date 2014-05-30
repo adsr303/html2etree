af4e0c1f167e8a25df3591962ea705cb6839ca50
Artur de Sousa Rocha <adsr@poczta.onet.pl>
2014-05-31 00:08:08 +0200

    Add basic docstrings

--- html2etree.py
+++ html2etree.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 
 
 class HTML2ETree(HTMLParser):
+    """HTML parser that attempts to build an ElementTree."""
 
     def __init__(self):
         HTMLParser.__init__(self)
@@ -13,12 +14,46 @@ class HTML2ETree(HTMLParser):
         self._text = None
         self._tail = None  # Last-ended element in scope, if any
 
+    @classmethod
+    def parse(cls, source):
+        """Parse an external HTML document into an ElementTree.
+
+        source - a filename or a file-like object.
+        """
+        try:
+            f = open(source)
+        except TypeError:
+            return cls.fromstringlist(source)
+        with f:
+            return cls.fromstringlist(f)
+
+    @classmethod
+    def fromstring(cls, text):
+        """Parse HTML into an ElementTree from string constant."""
+        return cls.fromstringlist([text])
+
+    @classmethod
+    def fromstringlist(cls, sequence):
+        """Parse HTML into an ElementTree from a sequence of strings."""
+        parser = cls()
+        for text in sequence:
+            if isinstance(text, bytes):
+                text = str(text, 'utf-8')  # TODO encoding?
+            parser.feed(text)
+        parser.close()
+        return parser._tree
+
+    def tree(self):
+        """Return the parsed ElementTree."""
+        return self._tree
+
     def handle_starttag(self, tag, attrs):
         self._stack.append(self._handle_start_sub(tag, attrs))
         self._tail = None
 
     def handle_endtag(self, tag):
         self._tail = self._stack.pop()
+        # TODO May need some heuristics to track back up (c.f. <br> or <hr>).
         assert self._tail.tag == tag
 
     def handle_startendtag(self, tag, attrs):
@@ -42,15 +77,11 @@ class HTML2ETree(HTMLParser):
     def handle_comment(self, data):
         self._handle_special_sub(ET.Comment(data))
 
-    def handle_decl(self, decl):
-        pass
+    #def handle_decl(self, decl):
+    #    pass
 
     def handle_pi(self, data):
         self._handle_special_sub(ET.ProcessingInstruction(data))
-
-    def tree(self):
-        """Return the parsed ElementTree."""
-        return self._tree
 
     def _top(self):
         """Return top element on stack."""
